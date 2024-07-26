1a41e8e6a89e54767045d618ff4df07e1ef15829
brigidro <valssss@yandex.ru>
2024-07-26 18:39:43 +0300

    kafkaConfig

---
+++ generator-data-kafka/src/main/java/com/example/generator_data_kafka/config/TextPath.java
@@ -0,0 +1,21 @@
+package com.example.generator_data_kafka.config;
+
+import com.jcabi.xml.XML;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class TextPath {
+
+
+    private final XML xml;
+    private final String node;
+    @Override
+    public String toString() {
+        return this.xml.nodes(this.node)
+                .get(0)
+                .xpath("text()")
+                .get(0);
+
+
+    }
+}
